1e53aa51bb03d08a0f1a70fbf8607cfff5b26538
Fabricio <102633447+Fabricio-Ricardo@users.noreply.github.com>
2023-04-14 18:39:47 -0300

    update 14/04/2023

--- arquivo-js/scroll.js
+++ arquivo-js/scroll.js
@@ -1,36 +1,33 @@
 window.sr = ScrollReveal({ reset: true})
 ScrollReveal().reveal('h1', {
-    delay: 700, duration:2000,
-        rotate:{x:0, y:100, z:0, duration:200}
+    delay: 500, duration:1000,
+        rotate:{x:0, y:100, z:0, duration:300}
 
 })
 
 ScrollReveal().reveal('.caixaServices', {
-    delay: 200, 
-    duration:2000,
+  
     origin:"top",
     distance:"50px",
-    duration:2000
+
         
 
 })
 
 ScrollReveal().reveal('.caixaServices2', {
-    delay: 600, 
-    duration:2000,
+    
     origin:"top",
     distance:"50px",
-    duration:2000
+    
         
 
 })
 
 ScrollReveal().reveal('.caixaServices3', {
-    delay: 1000, 
-    duration:2000,
+ 
     origin:"top",
     distance:"50px",
-    duration:2000
+   
         
 
 })
